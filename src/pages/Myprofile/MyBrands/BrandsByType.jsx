38f97a89eb1c6804b45bed21045381f689324f1e
pipi <pishuzhang@dingtalk.com>
2021-03-25 14:52:18 +0800

    My Brand edit add img update

--- src/pages/Myprofile/MyBrands/BrandsByType.jsx
+++ src/pages/Myprofile/MyBrands/BrandsByType.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useState, useEffect } from 'react'
 import { useHistory, useParams } from 'react-router'
 import styled from 'styled-components'
 import Modal from '@components/Modal/Modal'
-import { TextInput, TextAreaInput, Button, } from '@components/UI-kit'
+import { TextInput, TextAreaInput, Button, Upload } from '@components/UI-kit'
 import { myContext } from '@/redux'
 import useAxios from '@utils/useAxios.js'
 import { CardItem, AddCardItem } from './CardItem'
@@ -21,6 +21,7 @@ import img_example_3 from '@assets/images/example_3.svg'
 import { useBrandInfo } from './useHook'
 import Snackbar from '@material-ui/core/Snackbar';
 import UpdateTopBarImg from './updateTopBarImg'
+import { ImgToUrl } from '@/utils/imgToUrl'
 
 const BrandsByTypeStyled = styled.div`
     margin-bottom: 84px;
@@ -46,6 +47,7 @@ const BrandsByTypeStyled = styled.div`
         background: linear-gradient(154.16deg, #306AFF 6.12%, #3E74FE 49.44%, #003BD3 89.29%);
         position: relative;
         margin-top: 16px;
+        background-size: 100%!important;
         button{
             background: none;
             width: 124px;
@@ -210,6 +212,7 @@ export default function BrandsByType () {
     const { brandInfo, run } = useBrandInfo(brandId)
     const { state } = useContext(myContext)
     const { sign_Axios } = useAxios()
+    const [fileData, setFileData] = useState(null)
 
     const [openUpdateTopBarImg, setOpenUpdateTopBarImg] = useState(false)
     // ----edit----
@@ -219,6 +222,7 @@ export default function BrandsByType () {
         description: brandInfo.description,
         ownername: state.UserInfo.username,
         id: brandId | 0,
+        imgurl: '',
     })
     const [editBtnText, setEditBtnText] = useState('Save')
     const [inputDisable, setInputDisable] = useState(false)
@@ -229,10 +233,14 @@ export default function BrandsByType () {
         type: 'error',
     })
     const handelEditSubmit = async () => {
+        if (!editFormData.brandname || !editFormData.description) {
+            return
+        }
+        const imgurl = await ImgToUrl(sign_Axios, fileData)
         setBtnLock(true)
         setInputDisable(true)
         setEditBtnText('Submitting ...')
-        const response = await sign_Axios.post('/api/v2/main/auth/updatebrandbyid', editFormData)
+        const response = await sign_Axios.post('/api/v2/main/auth/updatebrandbyid', { ...editFormData, imgurl })
         setBtnLock(false)
         setInputDisable(false)
         setEditBtnText('Save')
@@ -312,7 +320,7 @@ export default function BrandsByType () {
                 </button>
             </div>
 
-            <div className='bg_wrapper' style={brandInfo.bandimgurl ? { backgroundSize: '100%', background: `url(${brandInfo.bandimgurl}) center center no-repeat` } : {}}>
+            <div className='bg_wrapper' style={brandInfo.bandimgurl ? { backgroundSize: '100%!important', background: `url(${brandInfo.bandimgurl}) center center no-repeat` } : {}}>
                 {/* <div className='bg_wrapper' style={{ backgroundSize: '100%', background: `url(https://market-test.bounce.finance/pngfileget/blob-1616642351.png) center center no-repeat` }}> */}
                 <button onClick={() => setOpenUpdateTopBarImg(true)}>
                     <img src={edit_white} alt="" />
@@ -334,6 +342,7 @@ export default function BrandsByType () {
                             ...editFormData,
                             brandname: brandInfo.brandname,
                             description: brandInfo.description,
+                            imgurl: brandInfo.imgurl
                         })
                         setIsEdit(true)
                     }}>
@@ -391,6 +400,13 @@ export default function BrandsByType () {
                             setEditFormData({ ...editFormData, description })
                         }}
                     />
+                    <Upload type='image'
+                        width='200px'
+                        height='200px'
+                        defaultValue={editFormData.imgurl}
+                        lockInput={inputDisable} infoTitle='browse Brand Photo' onFileChange={(formData) => {
+                            setFileData(formData)
+                        }} />
                     <div className="button_group">
                         <Button height='48px' width='302px' onClick={() => setIsEdit(false)}>Cancel</Button>
                         <Button disabled={btnLock} height='48px' width='302px' primary onClick={handelEditSubmit}>{editBtnText}</Button>
