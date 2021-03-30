f98f5b085cab8fbec08a8262f5d8ac171011e4a0
ZengYiyu <247460386@qq.com>
2021-03-30 18:04:59 +0800

    Removed capitalize in Page(Sell)

--- src/pages/Myprofile/MyInventory/components/InputPrice.jsx
+++ src/pages/Myprofile/MyInventory/components/InputPrice.jsx
@@ -165,7 +165,6 @@ const Wrapper = styled.div`
 		font-weight: bold;
 		font-size: 12px;
 		line-height: 15px;
-		text-transform: capitalize;
 		color: #1f191b;
 		opacity: 0.7;
 		display:block;
@@ -177,7 +176,6 @@ const Wrapper = styled.div`
 		font-weight: normal;
 		font-size: 13px;
 		line-height: 15px;
-		text-transform: capitalize;
 		color: #1f191b;
 		opacity: 0.5;
 		grid-area: notice;
