06bf4170cc9afdd50378b5c90b593f815d01518a
XuMinhui <yitian_2018@163.com>
2021-04-01 01:43:05 +0800

    reapir bug

--- src/pages/Myprofile/MyInventory/SellNFT.jsx
+++ src/pages/Myprofile/MyInventory/SellNFT.jsx
@@ -269,7 +269,7 @@ export default function SellNFT() {
 							fees={fees}
 							minPrice={minimumBid}
 							maxPrice={directPurchasePrice}
-							minIncr={minimumBid * 0.01}
+							minIncr={parseFloat(minimumBid * 0.05)}
 							amount={amount || 1}
 							newUnit={fixedSwap_Unit}
 						/>
