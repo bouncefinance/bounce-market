bf8747f01afbb0ffc2595b2f3a826d28b3fa6e7e
XuMinhui <yitian_2018@163.com>
2021-04-06 18:44:38 +0800

    repair bug

--- src/web3/index.js
+++ src/web3/index.js
@@ -2,13 +2,7 @@ import { useWeb3React } from '@web3-react/core'
 import Web3 from "web3";
 
 export function getContract(library, abi, address) {
-    let web3 = null
-    if(library){
-        web3 = new Web3(library.provider)
-    }else{
-        web3 = new Web3('https://rinkeby.infura.io/v3/8f6d3d5d7a1442a38d9e8050c31c1884')
-    }
-    // const web3 = new Web3(library.provider);
+    const web3 = new Web3(library.provider);
     return new web3.eth.Contract(abi, address)
 }
 
