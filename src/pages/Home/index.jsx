af359fc9886dcc3ebfb44d2f348aa80377925c76
XuMinhui <yitian_2018@163.com>
2021-04-08 20:53:19 +0800

    optimized page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -318,7 +318,7 @@ It shouldn’t be longer then ~20-30 sec.'
 
         <Link to="/Factory">
           <div className="left">
-            <h3>Create your unique NFT on Bounce Collectible</h3>
+            <h3>Create your unique NFT on fangible</h3>
             <img src={arrows_white} alt="" />
           </div>
         </Link>
