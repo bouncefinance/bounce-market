c70875c02ae9718939681178cc801f39d3f011b6
Aleksandr Ishchenko <aleksandr@ankr.com>
2021-04-08 14:14:24 +0300

    [FD-3386] Boilerplate

--- src/utils/useInitEffect.js
+++
@@ -1,7 +0,0 @@
-import { useEffect } from 'react';
-function useInitEffect(effect) {
-  // eslint-disable-next-line
-  return useEffect(effect, []);
-}
-
-export { useInitEffect };
--- src/utils/utils.js
+++
@@ -1,3 +0,0 @@
-export const getEllipsisAddress = (address) => {
-  return address.substring(0, 4) + '...' + address.slice(-4);
-}
