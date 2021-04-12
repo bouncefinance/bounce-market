fb6e6b65544bf9d455ffc8470d321696f3917981
XuMinhui <yitian_2018@163.com>
2021-04-12 12:18:12 +0800

    repair bug

--- src/utils/useAxios.js
+++ src/utils/useAxios.js
@@ -12,8 +12,9 @@ const Base_URL =
             'https://market-test.bounce.finance' :  // BSC Test https
             host.includes('127.0.0.1') ?
                 'https://bounce-market.bounce.finance' :    // BSC Main
-                'http://market-test.bounce.finance:11000'   // BSC Test http 
-// const Base_URL = 'https://bounce-market.bounce.finance'
+                // 'http://market-test.bounce.finance:11000'   // BSC Test http 
+                'https://bounce-market.bounce.finance'   // BSC Test http 
+                // const Base_URL = 'https://bounce-market.bounce.finance'
 
 const signStr = 'Welcome to Bounce!'
 let isRequestLock = false
