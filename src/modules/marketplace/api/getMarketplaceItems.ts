ed51018d4588426e1f1f2a9e51cb9eabd1d14462
Aleksandr Ishchenko <aleksandr@ankr.com>
2021-04-13 17:53:36 +0300

    [FD-3386] getMarketplaceItems

--- src/modules/marketplace/api/getMarketplaceItems.ts
+++ src/modules/marketplace/api/getMarketplaceItems.ts
@@ -1,5 +1,64 @@
 import { gql } from '@apollo/client';
 import { getApolloClient } from '../../common/api/getApolloClient';
+import { BigNumber } from 'bignumber.js';
+import Web3 from 'web3';
+
+interface IApiTradeAuction {
+  amountMin1: string;
+  createTime: number;
+  lastestBidAmount: string;
+  poolId: number;
+  state: 0;
+  token1: string;
+  tokenId: number;
+  __typename: 'TradeAuction';
+}
+
+interface ITradeAuction {
+  amountMin1: BigNumber;
+  createTime: Date;
+  lastestBidAmount: BigNumber;
+  poolId: number;
+  state: number;
+  token1: string;
+  tokenId: number;
+}
+
+function mapTradeAuction(data: IApiTradeAuction): ITradeAuction {
+  return {
+    amountMin1: new BigNumber(Web3.utils.fromWei(data.amountMin1)),
+    createTime: new Date(data.createTime),
+    lastestBidAmount: new BigNumber(Web3.utils.fromWei(data.lastestBidAmount)),
+    poolId: data.poolId,
+    state: data.state,
+    token1: data.token1,
+    tokenId: data.tokenId,
+  };
+}
+
+interface IApiTradePool {
+  createTime: number;
+  poolId: 0;
+  price: string;
+  state: 1;
+  token1: string;
+  tokenId: number;
+  __typename: 'TradePool';
+}
+
+interface ITradePool {
+  createTime: Date;
+  price: BigNumber;
+  tokenId: number;
+}
+
+function mapTradePool(data: IApiTradePool): ITradePool {
+  return {
+    createTime: new Date(data.createTime),
+    price: new BigNumber(Web3.utils.fromWei(data.price)),
+    tokenId: data.tokenId,
+  };
+}
 
 export const QueryTradePools = gql`
   query {
@@ -23,6 +82,21 @@ export const QueryTradePools = gql`
   }
 `;
 
-export async function getMarketplaceItems() {
-  return await getApolloClient().query({ query: QueryTradePools });
+interface IApiResponse {
+  tradeAuctions: IApiTradeAuction[];
+  tradePools: IApiTradePool[];
+}
+
+export async function getMarketplaceItems(): Promise<{
+  tradeAuctions: ITradeAuction[];
+  tradePools: ITradePool[];
+}> {
+  const query = await getApolloClient().query<IApiResponse>({
+    query: QueryTradePools,
+  });
+
+  return {
+    tradeAuctions: query.data.tradeAuctions.map(mapTradeAuction),
+    tradePools: query.data.tradePools.map(mapTradePool),
+  };
 }
--- src/modules/overview/screens/Overview/Overview.tsx
+++ src/modules/overview/screens/Overview/Overview.tsx
@@ -1,8 +1,15 @@
 import * as React from 'react';
 import { Typography } from '@material-ui/core';
 import { t } from '../../../i18n/utils/intl';
+import { useAppDispatch } from '../../../../store/useAppDispatch';
+import { useEffect } from 'react';
+import { MarketplaceActions } from '../../../marketplace/marketplaceActions';
 
 export const Overview = () => {
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(MarketplaceActions.fetchMarketplaceItems());
+  }, [dispatch]);
   return (
     <>
       <Typography>{t('foobar')}</Typography>
