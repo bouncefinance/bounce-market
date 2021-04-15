194b60a422849e5bbc2a5f44406d0de23cc738f6
ZengYiyu <247460386@qq.com>
2021-04-15 11:39:07 +0800

    Added Chinese in some modalMessage

--- src/locales/en_US.js
+++ src/locales/en_US.js
@@ -9,6 +9,11 @@ const en_US = {
     CopySuccessful: 'Copy Successful',
     TryAgain: 'Hmm. You hit a glitch. Sorry for the trouble. Please try again.',
     
+    userUserInfo: {
+        "uploadSuccess": "Information upload success!",
+        "OnlySupports": "Only Supports JPG, PNG, JPEG2000",
+    },
+
     ErrorMsg: {
         "required": "This field is required and cannot be blank",
         "regular": "This is not in conformity with the standard, please fill in again",
@@ -26,6 +31,7 @@ const en_US = {
         setting: 'Account Settings',
         search: 'Search',
         search_p: 'Search objects, items, art',
+        SelectBSC: "Please select BSC network.",
     },
 
     'pages.buy.Share': 'Share',
@@ -66,6 +72,8 @@ const en_US = {
     'pages.buy.AreYouSure': 'Are you sure to cancel?',
     'pages.buy.Yes': 'Yes',
     'pages.buy.No': 'No',
+    'pages.buy.TransactionSuccess': 'The transaction has been successfully completed！',
+    'pages.buy.AuctionCancelled': 'The auction bill can only be cancelled when it expires',
 
     'pages.buy.TradeTable.Event': 'Event',
     'pages.buy.TradeTable.Quantity': 'Quantity',
@@ -79,6 +87,7 @@ const en_US = {
     'pages.buy.TradeTable.Created': 'Created',
     'pages.buy.TradeTable.Listed': 'Listed',
 
+    "UIKit.Input.Upload.infoTip.FormatIncorrect": 'The file format you selected is incorrect',
     "UIKit.Input.Upload.infoTip.uploadImage": 'upload Image',
     "UIKit.Input.Upload.infoTip.requirement1": 'Supports JPG, PNG, JPEG2000',
     "UIKit.Input.Upload.infoTip.requirement2": 'no more than 100MB, 262*262 Reccomended',
--- src/pages/Myprofile/MyGallery/MyNFT.jsx
+++ src/pages/Myprofile/MyGallery/MyNFT.jsx
@@ -227,11 +227,11 @@ function MyNFT() {
 						setImgURL(NFTInfoList.fileurl);
 						setDescription(NFTInfoList.description || "")
 					} else {
-						dispatch({ type: 'Modal_Message', showMessageModal: true, modelType: 'error', modelMessage: wrapperIntl("MyProfile.MyGallery.MyNFT.DataUpdateFailed") });
+						dispatch({ type: 'Modal_Message', showMessageModal: true, modelType: 'error', modelMessage: wrapperIntl("TryAgain") });
 					}
 				})
 				.catch((err) => {
-					dispatch({ type: 'Modal_Message', showMessageModal: true, modelType: 'error', modelMessage: wrapperIntl("MyProfile.MyGallery.MyNFT.DataUpdateFailed") });
+					dispatch({ type: 'Modal_Message', showMessageModal: true, modelType: 'error', modelMessage: wrapperIntl("TryAgain") });
 				});
 		};
 		if (!active || !nftId) return;
