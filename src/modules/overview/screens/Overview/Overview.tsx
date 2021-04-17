be77c98cf4739e27b3f948e8f6a488d803646485
Aleksandr Ishchenko <aleksandr@ankr.com>
2021-04-18 01:04:20 +0300

    [FD-3386] Bind fast movers

--- src/modules/overview/screens/Overview/Overview.tsx
+++ src/modules/overview/screens/Overview/Overview.tsx
@@ -2,7 +2,7 @@ import { ThemeProvider } from '@material-ui/styles';
 import { useDispatchRequest } from '@redux-requests/react';
 import { Artists } from 'modules/overview/components/Artists';
 import { Brands } from 'modules/overview/components/Brands';
-import { Movers } from 'modules/overview/components/Movers';
+import { MoversComponent } from 'modules/overview/components/Movers';
 import { Promo } from 'modules/overview/components/Promo';
 import { darkTheme } from 'modules/themes/darkTheme';
 import React, { useEffect } from 'react';
@@ -12,9 +12,52 @@ import { INDEX_PATH } from '../../../router/const';
 import { t } from '../../../i18n/utils/intl';
 import { Queries } from '../../../common/components/Queries/Queries';
 
+function mapPromoItem(
+  item: Exclude<ReturnType<typeof useItems>['data'], null>[0],
+) {
+  return {
+    title: item.itemname || '',
+    text: item.itemname || '',
+    createdBy: item.itemname || '',
+    avatar: 'https://via.placeholder.com/40x40',
+    price: t('unit.BNB-value', {
+      value: item.price.toNumber(),
+    }),
+    img: item.fileurl,
+    thumbImg: item.fileurl || '',
+    href: INDEX_PATH,
+    authorHref: INDEX_PATH,
+  };
+}
+
+function mapMoversItem(
+  item: Exclude<ReturnType<typeof useItems>['data'], null>[0],
+) {
+  return {
+    title: item.itemname || '',
+    price: item.price,
+    priceType: 'USDT',
+    endDate: new Date(2021, 3, 18),
+    likes: 100,
+    href: '#',
+    img: item.fileurl || '',
+    ProfileInfoProps: {
+      subTitle: 'Owner',
+      title: '1livinginzen',
+      users: [
+        {
+          name: 'name',
+          avatar: 'https://via.placeholder.com/32',
+        },
+      ],
+    },
+  };
+}
+
 export const Overview = () => {
   const { data } = useItems();
   const promoItems = data?.splice(0, 3);
+  const fastMoversItems = data?.splice(3, 12);
   const dispatchRequest = useDispatchRequest();
   useEffect(() => {
     dispatchRequest(MarketplaceActions.fetchPools()).then(({ data }) => {
@@ -38,29 +81,24 @@ export const Overview = () => {
           ]}
         >
           {() => (
-            <Promo
-              stackDown
-              items={
-                promoItems?.map(item => ({
-                  title: item.itemname!,
-                  text: item.itemname!,
-                  createdBy: item.itemname!,
-                  avatar: 'https://via.placeholder.com/40x40',
-                  price: t('unit.BNB-value', {
-                    value: item.price.toNumber(),
-                  }),
-                  img: item.fileurl,
-                  thumbImg: item.fileurl!,
-                  href: INDEX_PATH,
-                  authorHref: INDEX_PATH,
-                })) || []
-              }
+            <Promo stackDown items={promoItems?.map(mapPromoItem) || []} />
+          )}
+        </Queries>
+      </ThemeProvider>
+      <ThemeProvider theme={darkTheme}>
+        <Queries
+          requestActions={[
+            MarketplaceActions.fetchPools,
+            MarketplaceActions.fetchItems,
+          ]}
+        >
+          {() => (
+            <MoversComponent
+              items={fastMoversItems?.map(mapMoversItem) || []}
             />
           )}
         </Queries>
       </ThemeProvider>
-
-      <Movers />
 
       <ThemeProvider theme={darkTheme}>
         <Artists />
