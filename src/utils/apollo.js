818f16ba2bc1cbd863d721276cf7c43cd86e2cb7
XuMinhui <yitian_2018@163.com>
2021-04-19 21:29:50 +0800

    rebuild

--- src/utils/apollo.js
+++ src/utils/apollo.js
@@ -8,6 +8,7 @@ export const client = new ApolloClient({
     // 'https://api.thegraph.com/subgraphs/name/winless/bouncenft' :      // bsc main
     // 'https://subgraph_bsc.bounce.finance/subgraphs/name/winless/bouncenft' :      // bsc main
     'https://subgraph_official_bsc.bounce.finance/subgraphs/name/winless/BounceNFT':
+    
     'https://subgraph_bsc.bounce.finance/subgraphs/name/winless/bouncenft2' ,      // bsc main
     // 'https://api.thegraph.com/subgraphs/name/winless/bouncenft2',     // bsc test
     // 'https://subgraph_bsc.bounce.finance/subgraphs/name/winless/bouncenft2',
