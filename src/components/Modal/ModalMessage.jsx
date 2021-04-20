a5e05090907aec0848fce249feda2876257871c2
ZengYiyu <247460386@qq.com>
2021-04-20 10:44:50 +0800

    Deleted 'Info' modalMessage after signing

--- src/components/Modal/ModalMessage.jsx
+++ src/components/Modal/ModalMessage.jsx
@@ -187,7 +187,7 @@ export default function ModalMessage() {
 						</IconButton>
 					}
 				>
-					{state.modelMessage || "Info"}
+					{state.modelMessage/*  || "Info" */}
           
 					<span
 						className={classes.links}
