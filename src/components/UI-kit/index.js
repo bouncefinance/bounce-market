61ea4109ea6779be45bcbaf5d8b3df96ca092b0f
chen <coder_chen@126.com>
2021-04-23 14:04:17 +0800

    new NFT upload

--- src/components/UI-kit/index.js
+++ src/components/UI-kit/index.js
@@ -5,6 +5,7 @@ import NumberInput from './Input/NumberInput'
 import TextInput from './Input/TextInput'
 import StubInput from './Input/StubInput'
 import Upload from './Input/Upload'
+import UploadAll from './Input/UploadAll'
 import TextAreaInput from './Input/TextAreaInput'
 import PullCheckBox from './Select/PullCheckBox'
 import PullRadioBox from './Select/PullRadioBox'
@@ -20,6 +21,7 @@ export {
     PullCheckBox,
     PullRadioBox,
     Upload,
+    UploadAll,
     Radio,
     StubInput
 }
