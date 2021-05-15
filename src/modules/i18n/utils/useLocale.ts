93cf9d24056a1edd07ad751ed1482d4cf4864137
xixe <xixedev@gmail.com>
2021-05-15 16:52:42 +0700

    [FE-3438] Language switcher, save in LocalStorage via redux-persist

--- src/modules/i18n/utils/useLocale.ts
+++ src/modules/i18n/utils/useLocale.ts
@@ -2,7 +2,6 @@ import { useAppSelector } from '../../../store/useAppSelector';
 
 export function useLocale() {
   return useAppSelector(({ i18n: { locale } }) => {
-    console.log({ locale });
     return { locale };
   });
 }
