efbf66f7b9035e494e28a89ebb84d2fb3e3bc7bd
Denise <41983880+DeniseTeafter@users.noreply.github.com>
2021-05-18 19:20:00 +0800

    [FD-3397] List brand items

---
+++ src/modules/brand/actions/listBrandItems.tsx
@@ -0,0 +1,92 @@
+import { DispatchRequest, RequestAction } from '@redux-requests/core';
+import { queryItemByFilter } from 'modules/pools/actions/queryItemByFilter';
+import { queryPools } from 'modules/pools/actions/queryPools';
+import { Store } from 'redux';
+import { createAction as createSmartAction } from 'redux-smart-actions';
+import { RootState } from 'store';
+import { compare } from '../api/queryBrand';
+import { ListBrandItemsAction } from './const';
+import { queryBrandItem1155, queryBrandItem721 } from './queryBrandItems';
+
+export const listBrandItems = createSmartAction(
+  ListBrandItemsAction,
+  ({ userAddress, contractAddress }) => ({
+    request: {
+      promise: (async function () { })(),
+    },
+    meta: {
+      asMutation: true,
+      onRequest: (
+        reqeust: { promise: Promise<any> },
+        action: RequestAction,
+        store: Store<RootState> & { dispatchRequest: DispatchRequest },
+      ) => {
+        return {
+          promise: (async () => {
+            const { data: items721 } = await store.dispatchRequest(
+              queryBrandItem721({
+                user_address: userAddress,
+                contract_address: contractAddress
+              })
+            )
+
+            const { data: items1155 } = await store.dispatchRequest(
+              queryBrandItem1155({
+                user_address: userAddress,
+                contract_address: contractAddress
+              })
+            )
+
+            const { data: pools } = await store.dispatchRequest(
+              queryPools(contractAddress)
+            )
+
+            const tradePools = pools?.tradePools?.filter(item => item.state !== 1 && compare(item.token0, contractAddress))
+              ?? [];
+            const tradeAuctions = pools?.tradeAuctions?.filter(item => item.state !== 1 && compare(item.token0, contractAddress))
+              ?? [];
+
+            const list = items721.concat(items1155).concat(tradePools).concat(tradeAuctions);
+
+            const ids = list.map((item: any) => item.tokenId || parseInt(item.token_id));
+            const cts = list.map((item: any) => item.token0 || item.contract_addr)
+
+            const { data: items } = await store.dispatchRequest(
+              queryItemByFilter({
+                ids: ids,
+                cts: cts,
+                category: '',
+                channel: '',
+              })
+            )
+
+            const poolList = list.map((item: any) => {
+              const poolInfo = items?.find((pool: any) => {
+                return (parseInt(item.token_id) === pool.id || item.tokenId === pool.id)
+                  && (
+                    compare(item.contract_addr, pool.contractaddress) ||
+                    compare(item.contract_addr, pool.token0)
+                  )
+              })
+
+              if (poolInfo) {
+                return {
+                  ...poolInfo,
+                  poolType: item && item.amount_total0 ? 'fixedSwap' : 'englishAuction',
+                  poolId: item && item.poolId,
+                  price: item && (item.lastestBidAmount || item.amountMin1),
+                  token1: item && item.token1,
+                  createTime: item && item.createTime
+                }
+              } else {
+                return null
+              }
+            })
+
+            return poolList;
+          })()
+        }
+      }
+    }
+  })
+)
--- src/modules/brand/screens/MyBrand/MyBrand.tsx
+++ src/modules/brand/screens/MyBrand/MyBrand.tsx
@@ -2,8 +2,12 @@ import { Container, Grid } from '@material-ui/core';
 import { useDispatchRequest } from '@redux-requests/react';
 import { useAccount } from 'modules/account/hooks/useAccount';
 import { getAccountBrand } from 'modules/brand/actions/getAccountBrand';
+import { listBrandItems } from 'modules/brand/actions/listBrandItems';
 import { IBrandInfo } from 'modules/brand/api/queryBrand';
 import { BrandAddItem } from 'modules/brand/components/BrandEmptyCard/BrandAddItem';
+import { BuyNFTRoutesConfig } from 'modules/buyNFT/BuyNFTRoutes';
+import { ProductCard } from 'modules/common/components/ProductCard';
+import { RoutesConfiguration } from 'modules/createNFT/Routes';
 import { Avatar } from 'modules/profile/components/Avatar';
 import { Header } from 'modules/profile/components/Header';
 import { InfoPanel } from 'modules/profile/components/InfoPanel';
@@ -16,7 +20,7 @@ export const MyBrand = () => {
   const dispatch = useDispatchRequest();
   const { address } = useAccount();
   const [brandInfo, setBrandInfo] = useState<IBrandInfo>();
-
+  const [items, setItems] = useState([])
   useEffect(() => {
     if (address) {
       dispatch(getAccountBrand(address))
@@ -29,10 +33,22 @@ export const MyBrand = () => {
     }
   }, [address, dispatch]);
 
+  useEffect(() => {
+    if (address && brandInfo) {
+      dispatch(listBrandItems({
+        userAddress: address,
+        contractAddress: brandInfo?.contractaddress
+      }))
+        .then(res => {
+          setItems(res.data)
+        })
+    }
+  }, [address, brandInfo, dispatch]);
+
   const handleEditImg = () => {
   }
 
-  return <Section>
+  return <Section className={classes.root}>
     <Header
       img={brandInfo?.bandimgurl}
       onEditClick={handleEditImg}
@@ -50,7 +66,44 @@ export const MyBrand = () => {
       />
 
       <Grid container spacing={4}>
-        {brandInfo?.id && <BrandAddItem id={brandInfo.id} />}
+        <div style={{padding:16}}>{brandInfo?.id && <BrandAddItem id={brandInfo.id} />}</div>
+        {items.map((item: any) => <Grid item xs={12} sm={6} lg={4} xl={3} key={item.id}>
+          <ProductCard
+            key={item.id}
+            title={item.itemname}
+            href={
+              item.poolId && item.poolType
+                ? BuyNFTRoutesConfig.DetailsNFT.generatePath(
+                  item.poolId,
+                  item.poolType,
+                )
+                : ''
+            }
+            price={item.poolId ? item.price : undefined}
+            copies={item.supply}
+            MediaProps={{
+              category: item.category,
+              src: item.fileurl,
+              objectFit: 'scale-down',
+              loading: 'lazy',
+            }}
+            ProfileInfoProps={{
+              subTitle: 'Owner',
+              title: '1livinginzen',
+              users: [
+                {
+                  name: 'name',
+                  avatar: 'https://via.placeholder.com/32',
+                  verified: true,
+                },
+              ],
+            }}
+            toSale={RoutesConfiguration.PublishNft.generatePath(
+              item.contractaddress,
+              item.id,
+            )}
+          />
+        </Grid>)}
       </Grid>
     </Container>
   </Section>
