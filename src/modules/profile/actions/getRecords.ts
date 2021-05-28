15b55c29b32444e72fbc92febc485ea5206d7727
Anton Panov <aliengrey88@gmail.com>
2021-05-28 20:10:31 +0300

    [FAN-129] Entry to claim (#51)
    * refactor: add time is over text in NFT card
    
    * refactor: move profile screen to own folder
    
    * improvement: increase brands page loading speed
    
    * feat: add profile bids

---
+++ src/modules/profile/actions/getRecords.ts
@@ -0,0 +1,61 @@
+import {
+  DispatchRequest,
+  getQuery,
+  RequestAction,
+  RequestActionMeta,
+} from '@redux-requests/core';
+import {
+  ISetAccountData,
+  setAccount,
+} from 'modules/account/store/actions/setAccount';
+import { FANGIBLE_URL } from 'modules/common/conts';
+import { Store } from 'redux';
+import { createAction } from 'redux-smart-actions';
+import { RootState } from 'store';
+import { IApiRecords, IRecords } from '../api/records';
+
+interface IGetRecordsArgs {
+  offset?: number;
+  count?: number;
+}
+
+export const getRecords = createAction<
+  RequestAction<IApiRecords, IRecords>,
+  [IGetRecordsArgs?, RequestActionMeta<IApiRecords, IRecords>?]
+>('getRecords', (params, meta) => ({
+  request: {
+    url: '/records',
+    method: 'get',
+    baseURL: FANGIBLE_URL,
+    params: {
+      offset: params?.offset || 0,
+      count: params?.count || 100,
+      user_address: '',
+    },
+  },
+  meta: {
+    ...meta,
+    driver: 'axios',
+    onRequest: (
+      request,
+      _action: RequestAction,
+      store: Store<RootState> & { dispatchRequest: DispatchRequest },
+    ) => {
+      const {
+        data: { address },
+      } = getQuery<ISetAccountData>(store.getState(), {
+        type: setAccount.toString(),
+      });
+
+      request.params.user_address = address;
+
+      return request;
+    },
+    getData: ({ code, msg, data }) => {
+      if (code !== 200) {
+        throw new Error(msg);
+      }
+      return data;
+    },
+  },
+}));
