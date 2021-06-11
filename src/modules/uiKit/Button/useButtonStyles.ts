c0bb02bfd2b817567c3a97bfdc8bff0f4627f726
Anton Panov <aliengrey88@gmail.com>
2021-06-12 00:39:42 +0300

    [FAN-206] Button loading state (#82)
    * feat: add button loading icon
    
    * feat: use button loading prop

--- src/modules/uiKit/Button/useButtonStyles.ts
+++ src/modules/uiKit/Button/useButtonStyles.ts
@@ -1,7 +1,56 @@
 import { makeStyles, Theme } from '@material-ui/core';
 
 export const useButtonStyles = makeStyles<Theme>(theme => ({
+  root: {
+    '& .MuiButton-startIcon, & .MuiButton-endIcon': {
+      transition: '0.2s',
+    },
+  },
+
   rounded: {
     borderRadius: 44,
+  },
+
+  loading: {
+    pointerEvents: 'none',
+
+    '&:active': {
+      transform: 'none',
+    },
+
+    '& .MuiButton-startIcon, & .MuiButton-endIcon': {
+      opacity: 0,
+      transform: 'scale(0.8)',
+    },
+  },
+
+  content: {
+    width: '100%',
+    display: 'inherit',
+    position: 'relative',
+    alignItems: 'inherit',
+    justifyContent: 'inherit',
+
+    transition: 'opacity 0.2s, transform 0.2s',
+  },
+
+  contentHidden: {
+    opacity: 0,
+    transform: 'scale(0.8)',
+  },
+
+  loadingIcon: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    marginTop: '-0.5em',
+    marginLeft: '-0.5em',
+    fontSize: 18,
+
+    '& svg': {
+      display: 'block',
+      width: '1em',
+      height: '1em',
+    },
   },
 }));
