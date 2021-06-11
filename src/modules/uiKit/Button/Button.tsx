1c82fc5ed829c63aac2c772d2650da9cf2f7e505
Antroll <aliengrey88@gmail.com>
2021-06-11 23:36:17 +0300

    feat: add button loading icon

--- src/modules/uiKit/Button/Button.tsx
+++ src/modules/uiKit/Button/Button.tsx
@@ -1,10 +1,12 @@
 import { Button as ButtonComponent, ButtonProps } from '@material-ui/core';
 import classNames from 'classnames';
-import { forwardRef } from 'react';
+import { QueryLoading } from 'modules/common/components/QueryLoading/QueryLoading';
+import { forwardRef, useCallback } from 'react';
 import { useButtonStyles } from './useButtonStyles';
 
 interface IButtonProps extends ButtonProps {
   rounded?: boolean;
+  loading?: boolean;
   [x: string]: any;
 }
 
@@ -13,14 +15,45 @@ interface IButtonProps extends ButtonProps {
  */
 // TODO: fix typings
 export const Button = forwardRef<HTMLButtonElement, IButtonProps>(
-  ({ rounded = false, className, ...props }, ref) => {
+  (
+    { rounded = false, className, loading, children, onClick, ...props },
+    ref,
+  ) => {
     const classes = useButtonStyles();
+
+    const onLoadingClick = useCallback((e: any) => {
+      e.preventDefault();
+    }, []);
+
     return (
       <ButtonComponent
         ref={ref}
-        className={classNames(className, rounded && classes.rounded)}
+        className={classNames(
+          className,
+          classes.root,
+          rounded && classes.rounded,
+          loading && classes.loading,
+        )}
         {...props}
-      />
+        onClick={loading ? onLoadingClick : onClick}
+      >
+        {children && (
+          <span
+            className={classNames(
+              classes.content,
+              loading && classes.contentHidden,
+            )}
+          >
+            {children}
+          </span>
+        )}
+
+        {loading && (
+          <i className={classes.loadingIcon}>
+            <QueryLoading />
+          </i>
+        )}
+      </ButtonComponent>
     );
   },
 );
