f855383405f5af08c3cc84b2d0ec3389aa5bacca
XuMinhui <yitian_2018@163.com>
2021-06-23 20:09:27 +0800

    Complete wandering market and brand without connection

--- src/modules/market/Routes.tsx
+++ src/modules/market/Routes.tsx
@@ -1,8 +1,6 @@
-import loadable, { LoadableComponent } from '@loadable/component';
-import { PrivateRoute } from 'modules/router/components/PrivateRoute';
 import React from 'react';
-import { QueryLoadingAbsolute } from '../common/components/QueryLoading/QueryLoading';
 import { RouteConfiguration } from '../common/types/RouteConfiguration';
+import { Market } from './screens/Market';
 
 const PATH_MARKET = '/market';
 
@@ -13,19 +11,6 @@ export const MarketRoutesConfig: { [key: string]: RouteConfiguration } = {
   },
 };
 
-const LoadableContainer: LoadableComponent<any> = loadable(
-  async () => import('./screens/Market').then(module => module.Market),
-  {
-    fallback: <QueryLoadingAbsolute />,
-  },
-);
-
 export function MarketRoutes() {
-  return (
-    <PrivateRoute
-      path={MarketRoutesConfig.Market.path}
-      exact={true}
-      component={LoadableContainer}
-    />
-  );
+  return <Market />;
 }
