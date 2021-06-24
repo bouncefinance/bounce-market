60a06097e32f01203e990688182ad0b93dc20183
XuMinhui <yitian_2018@163.com>
2021-06-24 18:42:22 +0800

    add loadable in market routes

--- src/modules/market/Routes.tsx
+++ src/modules/market/Routes.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import { RouteConfiguration } from '../common/types/RouteConfiguration';
-import { Market } from './screens/Market';
+import { Route } from 'react-router-dom';
+import { QueryLoadingAbsolute } from 'modules/common/components/QueryLoading/QueryLoading';
+import loadable, { LoadableComponent } from '@loadable/component';
 
 const PATH_MARKET = '/market';
+
+const LoadableContainer: LoadableComponent<any> = loadable(
+  async () => import('./screens/Market').then(module => module.Market),
+  {
+    fallback: <QueryLoadingAbsolute />,
+  },
+);
 
 export const MarketRoutesConfig: { [key: string]: RouteConfiguration } = {
   Market: {
@@ -12,5 +21,11 @@ export const MarketRoutesConfig: { [key: string]: RouteConfiguration } = {
 };
 
 export function MarketRoutes() {
-  return <Market />;
+  return (
+    <Route
+      path={MarketRoutesConfig.Market.path}
+      exact={true}
+      component={LoadableContainer}
+    />
+  );
 }
