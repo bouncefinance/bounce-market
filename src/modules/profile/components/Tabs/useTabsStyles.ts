8c74b6f3f7d9471be2105952bb84abb4edfc9e5f
Antroll <aliengrey88@gmail.com>
2021-06-28 18:03:38 +0300

    fix: increase the priority of styles for tabs

--- src/modules/profile/components/Tabs/useTabsStyles.ts
+++ src/modules/profile/components/Tabs/useTabsStyles.ts
@@ -1,38 +1,41 @@
 import { fade, makeStyles, Theme } from '@material-ui/core';
 
-export const useTabsStyles = makeStyles<Theme>(theme => ({
-  root: {
-    position: 'relative',
+export const useTabsStyles = makeStyles<Theme>(
+  theme => ({
+    root: {
+      position: 'relative',
 
-    '&:after': {
-      content: `''`,
-      position: 'absolute',
-      bottom: 0,
-      left: 0,
-      width: '100%',
-      borderBottom: `1px solid ${fade(theme.palette.text.primary, 0.1)}`,
+      '&:after': {
+        content: `''`,
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        width: '100%',
+        borderBottom: `1px solid ${fade(theme.palette.text.primary, 0.1)}`,
+      },
     },
-  },
 
-  tab: {
-    fontSize: 18,
-  },
+    tab: {
+      fontSize: 18,
+    },
 
-  tabWrapper: {
-    flexDirection: 'row',
-  },
+    tabWrapper: {
+      flexDirection: 'row',
+    },
 
-  tabCount: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: 23,
-    minWidth: 23,
-    padding: theme.spacing(0, 0.75),
-    marginLeft: theme.spacing(0.75),
-    border: `1px solid ${fade(theme.palette.text.primary, 0.1)}`,
-    borderRadius: 23,
-    fontSize: 12,
-    fontWeight: 700,
-  },
-}));
+    tabCount: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: 23,
+      minWidth: 23,
+      padding: theme.spacing(0, 0.75),
+      marginLeft: theme.spacing(0.75),
+      border: `1px solid ${fade(theme.palette.text.primary, 0.1)}`,
+      borderRadius: 23,
+      fontSize: 12,
+      fontWeight: 700,
+    },
+  }),
+  { index: 1 },
+);
