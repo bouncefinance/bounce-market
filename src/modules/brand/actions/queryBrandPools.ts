cb7ae3e504c809e254f9b020694082f83edbd5b2
XuMinhui <yitian_2018@163.com>
2021-07-02 20:10:41 +0800

    Add a detail jump to a single item in the brand list

--- src/modules/brand/actions/queryBrandPools.ts
+++ src/modules/brand/actions/queryBrandPools.ts
@@ -1,4 +1,6 @@
 import { DispatchRequest, RequestAction } from '@redux-requests/core';
+import { BuyNFTRoutesConfig } from 'modules/buyNFT/BuyNFTRoutes';
+import { AuctionType } from 'modules/overview/api/auctionType';
 import { queryItemByFilter } from 'modules/pools/actions/queryItemByFilter';
 import { queryPools } from 'modules/pools/actions/queryPools';
 import { Store } from 'redux';
@@ -10,6 +12,12 @@ import { QueryBrandPoolsAction } from './const';
 export interface IQueryBrandPoolsArgs {
   owneraddress: string;
   contractaddress: string;
+}
+
+interface ITempToken {
+  tokenId: number;
+  auctionType: AuctionType;
+  poolId: number;
 }
 
 export const queryBrandPools = createSmartAction<RequestAction>(
@@ -42,23 +50,82 @@ export const queryBrandPools = createSmartAction<RequestAction>(
                 compare(item.token0, params.contractaddress),
             );
 
-            const fsToken = fsPools.map(item => item.tokenId);
-            const eaToken = eaPools.map(item => item.tokenId);
-            const tokenList = fsToken.concat(eaToken);
+            const fsToken: ITempToken[] = fsPools.map(item => {
+              return {
+                tokenId: item.tokenId,
+                auctionType: AuctionType.FixedSwap,
+                poolId: item.poolId,
+              };
+            });
+            const eaToken: ITempToken[] = eaPools.map(item => {
+              return {
+                tokenId: item.tokenId,
+                auctionType: AuctionType.EnglishAuction,
+                poolId: item.poolId,
+              };
+            });
 
-            if (tokenList.length > 0) {
+            const allToken: ITempToken[] = fsToken.concat(eaToken);
+
+            const querytarTokenList = async (tarTokenList: ITempToken[]) => {
               const { data: itemData } = await store.dispatchRequest(
                 queryItemByFilter({
                   accountaddress: '',
                   category: '',
                   channel: '',
-                  cts: new Array(tokenList.length).fill(params.contractaddress),
-                  ids: tokenList,
+                  cts: new Array(tarTokenList.length).fill(
+                    params.contractaddress,
+                  ),
+                  ids: tarTokenList.map(item => item.tokenId),
                 }),
               );
-              return new Promise((resolve, reject) => resolve(itemData));
-            } else {
+
+              if (itemData?.length) {
+                return itemData.map(item => {
+                  const findTar = tarTokenList.find(
+                    tarItem => tarItem.tokenId === item.id,
+                  );
+                  return findTar
+                    ? {
+                        ...findTar,
+                        ...item,
+                        href:
+                          findTar.poolId && findTar.auctionType
+                            ? BuyNFTRoutesConfig.DetailsNFT.generatePath(
+                                findTar.poolId,
+                                findTar.auctionType,
+                              )
+                            : '',
+                      }
+                    : {};
+                });
+              } else {
+                return [];
+              }
+            };
+
+            if (allToken.length === 0) {
               return new Promise((resolve, reject) => resolve([]));
+            } else {
+              const PromiseList = [];
+
+              if (fsToken.length > 0) {
+                const queryFsList = querytarTokenList(fsToken);
+                PromiseList.push(queryFsList);
+              }
+
+              if (eaToken.length > 0) {
+                const queryEaList = querytarTokenList(eaToken);
+                PromiseList.push(queryEaList);
+              }
+
+              return Promise.all(PromiseList).then(res => {
+                const resArr: any[] | PromiseLike<any[]> = [];
+                for (let i = 0; i < res.length; i++) {
+                  resArr.push(...res[i]);
+                }
+                return resArr;
+              });
             }
           })(),
         };
