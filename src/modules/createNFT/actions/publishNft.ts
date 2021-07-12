0942470ddabd42c80c00779a6e43343df0bd5135
xxy <abc_xf@126.com>
2021-07-12 21:59:30 +0800

    feat: pulish nft time add bsc

--- src/modules/createNFT/actions/publishNft.ts
+++ src/modules/createNFT/actions/publishNft.ts
@@ -29,7 +29,9 @@ export const getFixedSwapContract = (chainID: number, isTime = false) => {
         ? process.env.REACT_APP_FIXED_CONTRACT_ADDRESS_RINKEBY_TIME
         : process.env.REACT_APP_FIXED_CONTRACT_ADDRESS_RINKEBY;
     case 56:
-      return process.env.REACT_APP_FIXED_CONTRACT_ADDRESS;
+      return isTime
+        ? process.env.REACT_APP_FIXED_CONTRACT_ADDRESS_TIME
+        : process.env.REACT_APP_FIXED_CONTRACT_ADDRESS;
     case 128:
       return process.env.REACT_APP_FIXED_CONTRACT_ADDRESS_HECO;
     default:
@@ -46,7 +48,9 @@ export const getEnglishAuctionContract = (chainID: number, isTime = false) => {
         ? process.env.REACT_APP_ENGLISH_AUCTION_CONTRACT_ADDRESS_RINKEBY_TIME
         : process.env.REACT_APP_ENGLISH_AUCTION_CONTRACT_ADDRESS_RINKEBY;
     case 56:
-      return process.env.REACT_APP_ENGLISH_AUCTION_CONTRACT_ADDRESS;
+      return isTime
+        ? process.env.REACT_APP_ENGLISH_AUCTION_CONTRACT_ADDRESS
+        : process.env.REACT_APP_ENGLISH_AUCTION_CONTRACT_ADDRESS_TIME;
     case 128:
       return process.env.REACT_APP_ENGLISH_AUCTION_CONTRACT_ADDRESS_HECO;
     default:
