b62c90dcb3cd53ef14277181b9c357e18aaf8e81
Denise <teafternoon@163.com>
2021-07-14 12:15:00 +0800

    [FAN-201] Set validation as bid ≤ Reserve price < Direct

--- src/modules/createNFT/screens/PublishNFT/PublishNFT.tsx
+++ src/modules/createNFT/screens/PublishNFT/PublishNFT.tsx
@@ -179,7 +179,7 @@ export const PublishNFTComponent = ({
           }
         }
 
-        if (!(minBid <= reservePrice && reservePrice <= purchasePrice)) {
+        if (!(minBid <= reservePrice && reservePrice < purchasePrice)) {
           errors.minBid = t(
             'publish-nft.error.wrong-direct-reserve-bid-amount',
           );
