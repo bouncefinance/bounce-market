634c51572ade1aaca109b151093ea5ecd3d570fe
Xxy-正经修行人 <xxy10062@gmail.com>
2021-08-31 23:39:17 +0800

    Hot/drops open (#293)
    * fix: drop open

--- src/modules/drops/screens/Drops/Drops.tsx
+++ src/modules/drops/screens/Drops/Drops.tsx
@@ -1,31 +1,16 @@
 import React from 'react';
 import { ThemeProvider } from '@material-ui/core';
 import { darkTheme } from 'modules/themes/darkTheme';
-import { Img } from 'modules/uiKit/Img';
 import { DropsSection } from './components/DropsSection';
 import { StoriesSlider } from './components/StoriesSlider';
 import { TopCreators } from './components/TopCreators';
-import TempBg from './tempBg.png';
 
 export const Drops = () => {
-  const isDev = true;
   return (
     <ThemeProvider theme={darkTheme}>
-      {isDev ? (
-        <>
-          <StoriesSlider />
-          <TopCreators />
-          <DropsSection />
-        </>
-      ) : (
-        <div style={{ textAlign: 'center' }}>
-          <Img
-            src={TempBg}
-            alt=""
-            style={{ maxWidth: '1180px', margin: '20px auto' }}
-          />
-        </div>
-      )}
+      <StoriesSlider />
+      <TopCreators />
+      <DropsSection />
     </ThemeProvider>
   );
 };
