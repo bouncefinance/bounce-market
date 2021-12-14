dccb3f6df69a2b0ffdfcf07440370e0b8c0d7f39
XuMinhui <yitian_2018@163.com>
2021-12-14 16:55:05 +0800

    hotfix/ repeat loge route

--- src/modules/layout/components/Logo/Logo.tsx
+++ src/modules/layout/components/Logo/Logo.tsx
@@ -1,13 +1,14 @@
 import { Link } from 'react-router-dom';
 import { ReactComponent as LogoImg } from './assets/logo.svg';
 import { useLogoStyles } from './LogoStyles';
-import { RoutesConfiguration } from '../../../overview/Routes';
+// import { RoutesConfiguration } from '../../../overview/Routes';
 
 export const Logo = () => {
   const classes = useLogoStyles();
   return (
     <Link
-      to={RoutesConfiguration.Overview.generatePath()}
+      // to={RoutesConfiguration.Overview.generatePath()}
+      to={'/drops'}
       className={classes.root}
     >
       <LogoImg className={classes.img} />
