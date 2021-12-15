057edbb3e872aacb29488c31e0a0ad0bd3ebe2b1
XuMinhui <yitian_2018@163.com>
2021-12-15 17:17:07 +0800

    hotfix/ open direct  link

--- src/modules/drops/actions/mockActive.ts
+++ src/modules/drops/actions/mockActive.ts
@@ -31,7 +31,8 @@ export const mockActive = [
     username: '0xc6a34b2bf59baF984884A0cf4C84eD1541E710d7',
     videourl: '',
     website: '',
-    // directLink: 'https://stage.fangible.com/profile/address/0x388651742c4e61e2f16999cb92dd8bf36bd360f9?id=&tab=owned',
+    directLink:
+      'https://stage.fangible.com/profile/address/0x388651742c4e61e2f16999cb92dd8bf36bd360f9?id=&tab=owned',
     tarChain: 4 as ChainType,
   },
   {
@@ -61,7 +62,8 @@ export const mockActive = [
     username: '0x8031957cEecEe4Dbd63935def5745940D6d75Bc6',
     videourl: '',
     website: '',
-    // directLink: 'https://stage-solona-guwkoimwk5.ccian.cc/profile/address/4fSBkyZDQ35uxYnBf42pEPk3e4u4JBWBRM7shJvD97yf?id=&tab=owned',
+    directLink:
+      'https://stage-solona-guwkoimwk5.ccian.cc/profile/address/4fSBkyZDQ35uxYnBf42pEPk3e4u4JBWBRM7shJvD97yf?id=&tab=owned',
     tarChain: 1111 as ChainType, // 1111 是 Solana
     comingTime: 'Coming Early 2022',
   },
