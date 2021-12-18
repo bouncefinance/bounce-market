cd29e8a748e8dd8636c2544acc5c2c183b842bed
xxy <xxy10062@gmail.com>
2021-12-18 16:18:16 +0800

    fix: add web3modal clover

--- src/modules/account/api/connectWallet.ts
+++ src/modules/account/api/connectWallet.ts
@@ -1,8 +1,9 @@
-import Web3Modal, { IProviderOptions } from 'web3modal';
+import Web3Modal, { IProviderOptions } from './web3modal';
 import { PALETTE } from '../../themes/mainTheme';
 import { fade, lighten } from '@material-ui/core';
 import WalletConnectProvider from '@walletconnect/web3-provider';
 import binanceWalletLogo from '../assets/binanceWallet.svg';
+import cloverWalletLogo from '../assets/clover.svg';
 import { BscConnector } from '@binance-chain/bsc-connector';
 import Web3 from 'web3';
 import { t } from '../../i18n/utils/intl';
@@ -12,7 +13,7 @@ export const RPC = {
   4: 'https://rinkeby.infura.io/v3/8f6d3d5d7a1442a38d9e8050c31c1884',
   56: 'https://bsc-dataseed4.binance.org',
 };
-
+const OPEN_CLOVER = false;
 export async function connectWallet(cacheProvider?: boolean) {
   const providerOptions: IProviderOptions = {
     ...(window.BinanceChain
@@ -33,6 +34,25 @@ export async function connectWallet(cacheProvider?: boolean) {
               });
               await connector.activate();
               return await connector.getProvider();
+            },
+          },
+        }
+      : {}),
+    ...(OPEN_CLOVER
+      ? {
+          'custom-clover': {
+            display: {
+              logo: cloverWalletLogo,
+              name: t('connect-wallet.custom-clover.name'),
+              description: t('connect-wallet.custom-clover.description'),
+            },
+            package: {
+              required: [],
+            },
+            options: {},
+            connector: async () => {
+              // TODO
+              return {};
             },
           },
         }
