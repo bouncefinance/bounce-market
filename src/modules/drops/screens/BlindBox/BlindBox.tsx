621e4a2f960584a1d111bc3e29d305a20446bcaf
xxy <xxy10062@gmail.com>
2022-02-16 08:56:14 +0800

    style: blindbox

--- src/modules/drops/screens/BlindBox/BlindBox.tsx
+++ src/modules/drops/screens/BlindBox/BlindBox.tsx
@@ -1,11 +1,34 @@
-import { Box, Button, ThemeProvider, Typography } from "@material-ui/core"
+import { Box, Button, ThemeProvider, Typography, useMediaQuery } from "@material-ui/core"
+import { useTheme } from '@material-ui/core/styles';
 import { darkTheme } from "modules/themes/darkTheme"
 import { Img } from "modules/uiKit/Img"
 import { useBlindBoxStyles } from "./useBlinkBoxStyles"
 import topImg from './assets/blindBoxBg.png'
+import { LayersIcon } from "modules/common/components/Icons/LayersIcon";
 
 export const BlindBox = () => {
     const classes = useBlindBoxStyles({})
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
+    const Detail = () => {
+        return <>
+        <Box mt={2} mb={2.4}>
+            <Typography className={classes.desc}>
+                Since then, Space Yacht has used the Twerk Skeleton design on both apparel and live visuals at their events. Once Space Yacht started producing NFTs, the legend of the Twerk Skeleton took on a whole new life of its own.
+            </Typography>
+        </Box>
+        <Box mb={ isMobile ? 2 : 4} display="flex">
+            <LayersIcon
+            className={classes.icon}
+            />
+            <Typography className={classes.soldNumber}>&nbsp;&nbsp; 10 / 1000</Typography>
+        </Box>
+            <Box display="flex" justifyContent="space-between" alignItems="center">
+                <Typography className={classes.price}>20.00 BNB / DRAW</Typography>
+                <Button style={{flex: 1, maxWidth: 350, marginLeft: 15}}>DRAW</Button>
+            </Box>
+        </>
+    }
     return <ThemeProvider theme={darkTheme}>
         <div className={classes.root}>
             <img alt="" className={classes.headTopImg} src={topImg} />
@@ -13,24 +36,22 @@ export const BlindBox = () => {
                 <Box mt={5} mb={4}>
                     <Typography variant="h2" >🔥Live</Typography>
                 </Box>
+                {[1, 2].map((id) => {
+                    return <Box className={classes.itemBox} mb={4.7}>
+                        <Box display="flex">
+                            <Img className={classes.boxLeft} src="" />
+                            <Box ml={6} className={classes.rightBox}>
+                                <Typography className={classes.time}>End: 10h 23m 19s</Typography>
+                                <Box mt={2.5}>
+                                    <Typography variant={isMobile ? 'h2' : "h1"} >AFT x NFT SELL</Typography>
+                                </Box>
+                                {!isMobile && <Detail />}
+                            </Box>
+                        </Box>
+                        {isMobile && <Detail />}
+                    </Box>
+                })}
             </div>
-            {[1,2].map((id) => {
-                return <Box mb={4.7} display="flex" >
-                    <Img className={classes.boxLeft} src="" />
-                    <div>
-                        <Typography>End: 10h 23m 19s</Typography>
-                        <Typography variant="h3" >AFT x NFT SELL</Typography>
-                        <Box>
-                        Since then, Space Yacht has used the Twerk Skeleton design on both apparel and live visuals at their events. Once Space Yacht started producing NFTs, the legend of the Twerk Skeleton took on a whole new life of its own.
-                        </Box>
-                        <Typography>10/1000</Typography>
-                        <Box display="flex" justifyContent="space-betweent">
-                            <span>20.00 BNB / DRAW</span>
-                            <Button>DRAW</Button>
-                        </Box>
-                    </div>
-                </Box>
-            })}
         </div>
     </ThemeProvider>
 }
--- src/modules/drops/screens/BlindBox/useBlinkBoxStyles.ts
+++ src/modules/drops/screens/BlindBox/useBlinkBoxStyles.ts
@@ -16,9 +16,60 @@ export const useBlindBoxStyles = makeStyles<Theme, { bgColor?: string }>(theme =
         margin: 'auto',
         padding: '0px 20px',
     },
+    itemBox: {
+        backgroundColor: '#232323',
+        borderRadius: 18,
+        overflow: 'hidden',
+        padding:  '70px',
+        [theme.breakpoints.down('sm')]: {
+            padding:  '30px',
+            borderRadius: 10,
+        }
+    },
     boxLeft: {
-        maxwidth: 338,
-        maxHeight: 338,
-        width: '20%',
+        width: 338,
+        height: 338,
+        borderRadius: 14,
+        overflow: 'hidden',
+        [theme.breakpoints.down('sm')]: {
+            // padding:  '30px',
+            borderRadius: 10,
+            width: 120,
+            height: 120,
+        }
+    },
+    rightBox: {
+        flex: 1,
+        [theme.breakpoints.down('sm')]: {
+            marginLeft: 25
+        }
+    },
+    time: {
+        display: 'inline-block',
+        padding: '21px 25px',
+        lineHeight: 0,
+        fontSize: 18,
+        borderRadius: 36,
+        whiteSpace: 'nowrap',
+        backgroundColor: 'rgba(0, 0, 0, 0.35)',
+        [theme.breakpoints.down('sm')]: {
+            fontSize: 14,
+            padding: '16px 18px',
+        }
+    },
+    desc: {
+        color: '#fff',
+        opacity: 0.7,
+        fontSize: 16
+    },
+    soldNumber: {
+        fontSize: 18,
+    },
+    price: {
+        fontSize: 24,
+        fontWeight: 600,
+        [theme.breakpoints.down('sm')]: {
+            fontSize: 16,
+        }
     }
 }))
